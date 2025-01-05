6cba52fc07485a6ffd66728fb8a34ca14da78797
Marquis <abahmarquis@gmail.com>
2025-01-04 18:45:03 -0800

    update code o add tooltip to messages sidebar icons

---
+++ components/NavTooltip.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="relative group">
+      <div class="p-2 cursor-pointer flex justify-center items-center mx-3" :class="{ 'bg-[#5B8469] rounded-lg': isActive }">
+        <NuxtLink :to="to" class="relative">
+          <slot></slot>
+          <!-- Tooltip -->
+          <div class="invisible group-hover:visible absolute left-full ml-2 px-6 py-2.5 bg-gray-800 text-white text-xs rounded whitespace-nowrap">
+            {{ text }}
+          </div>
+        </NuxtLink>
+      </div>
+    </div>
+  </template>
+  
+  <script lang="ts" setup>
+  defineProps<{
+    to: string;
+    text: string;
+    isActive?: boolean;
+  }>();
+  </script>
